9a5eda672d7733091b1e0b2d76a7511df4b00130
holecekp <24872113+holecekp@users.noreply.github.com>
2026-02-08 20:10:39 +0100

    Added interfaces for parsing a fuzzy number and formatting it as a string

---
+++ Interfaces/IFuzzyNumberFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+using Holecek.FuzzyMath.FuzzyNumbers;
+
+namespace FuzzyNumbers.Blazor.Interfaces;
+
+public interface IFuzzyNumberFormatter
+{
+    char BreakPointsSeparators { get; set; }
+    CultureInfo Culture { get; set; }
+
+    string Format(FuzzyNumber? fuzzyNumber);
+}
---
+++ Interfaces/IFuzzyNumberParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+using Holecek.FuzzyMath.FuzzyNumbers;
+
+namespace FuzzyNumbers.Blazor.Interfaces;
+
+public interface IFuzzyNumberParser
+{
+    char BreakPointsSeparators { get; set; }
+    CultureInfo Culture { get; set; }
+    bool TryPare(string input, out FuzzyNumber? fuzzyNumber);
+}
