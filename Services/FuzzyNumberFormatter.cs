0ac713c1709e65c1c308523bbf9148b2f1e43068
holecekp <24872113+holecekp@users.noreply.github.com>
2026-03-01 20:39:52 +0100

    Implemented class for formatting of fuzzy numbers as strings

---
+++ Services/FuzzyNumberFormatter.cs
@@ -0,0 +1,28 @@
+﻿using FuzzyNumbers.Blazor.Interfaces;
+using Holecek.FuzzyMath.FuzzyNumbers;
+using System.Globalization;
+
+namespace FuzzyNumbers.Blazor.Services;
+
+public class FuzzyNumberFormatter : IFuzzyNumberFormatter
+{
+    public char BreakPointsSeparators { get; set; } = ',';
+    public CultureInfo Culture { get; set; } = CultureInfo.InvariantCulture;
+
+    public string Format(FuzzyNumber? fuzzyNumber)
+    {
+        if (fuzzyNumber is null)
+        {
+            return string.Empty;
+        }
+        else
+        {
+            var alphaCuts = fuzzyNumber.AlphaCuts;
+            List<string> breakpoints = BreakPointsConverter.ConvertFromAlphaCuts(alphaCuts.ToList())
+                .Select(breakpoint => breakpoint.ToString(Culture))
+                .ToList();
+
+            return string.Join($"{BreakPointsSeparators} ", breakpoints);
+        }
+    }
+}
