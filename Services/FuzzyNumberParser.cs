3a5c0b500247aa51bda8b91ae6a9eeed56e92fcd
holecekp <24872113+holecekp@users.noreply.github.com>
2026-03-01 20:46:57 +0100

    Implemented class for parsing fuzzy numbers from strings

---
+++ Services/FuzzyNumberParser.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using Holecek.FuzzyMath.FuzzyNumbers;
+using FuzzyNumbers.Blazor.Interfaces;
+
+namespace FuzzyNumbers.Blazor.Services;
+
+public class FuzzyNumberParser : IFuzzyNumberParser
+{
+    public char BreakPointsSeparators { get; set; } = ',';
+
+    public CultureInfo Culture { get; set; } = CultureInfo.InvariantCulture;
+
+    public bool TryPare(string input, out FuzzyNumber? fuzzyNumber)
+    {
+        fuzzyNumber = null;
+
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+
+        try
+        {
+            List<double> breakPoints = input
+                .Split(BreakPointsSeparators, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => double.Parse(s.Trim(), Culture))
+                .ToList();
+
+            var alphaCuts = BreakPointsConverter.ConvertToAlphaCuts(breakPoints);
+            fuzzyNumber = new FuzzyNumber(alphaCuts);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
