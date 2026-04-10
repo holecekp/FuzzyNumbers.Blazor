abfb61532b6b9eb3bafb722d598779d41ee42377
holecekp <24872113+holecekp@users.noreply.github.com>
2026-04-10 21:11:39 +0200

    Model class moved to a separate file

---
+++ Models/FuzzyArithmeticsFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Holecek.FuzzyMath.FuzzyNumbers;
+
+namespace FuzzyNumbers.Blazor.Models;
+
+public class FuzzyArithmeticsFormModel
+{
+    [Required]
+    public FuzzyNumber FirstFuzzyNumber { get; set; } = new FuzzyNumber(1, 2, 3);
+
+    [Required]
+    public FuzzyNumber SecondFuzzyNumber { get; set; } = new FuzzyNumber(2, 3, 4, 5);
+}
