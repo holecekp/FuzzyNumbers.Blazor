0e32cddca207cc7d24b1d2ea6d8112beeb06316e
holecekp <24872113+holecekp@users.noreply.github.com>
2026-04-10 21:26:18 +0200

    Preparation for supporting other arithmetic operations than addition

--- Models/FuzzyArithmeticsFormModel.cs
+++ Models/FuzzyArithmeticsFormModel.cs
@@ -3,6 +3,14 @@ using Holecek.FuzzyMath.FuzzyNumbers;
 
 namespace FuzzyNumbers.Blazor.Models;
 
+public enum ArithmeticOperation
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
+
 public class FuzzyArithmeticsFormModel
 {
     [Required]
@@ -10,4 +18,17 @@ public class FuzzyArithmeticsFormModel
 
     [Required]
     public FuzzyNumber SecondFuzzyNumber { get; set; } = new FuzzyNumber(2, 3, 4, 5);
+
+    public ArithmeticOperation ArithmeticOperation { get; set; } = ArithmeticOperation.Addition;
+
+    public string ArithmeticOperationSign => ArithmeticOperation switch
+    {
+        ArithmeticOperation.Addition => "+",
+        ArithmeticOperation.Subtraction => "-",
+        ArithmeticOperation.Multiplication => "⋅",
+        ArithmeticOperation.Division => "/",
+        _ => throw new ArgumentOutOfRangeException()
+    };
+
+    public string ArithmeticOperationFormula => $"A{ArithmeticOperationSign}B";
 }
